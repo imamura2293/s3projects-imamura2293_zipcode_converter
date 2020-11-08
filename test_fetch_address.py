bf05586d92527fb3c0f783129962b9357891b26a
imamura2293 <uu77ee22dd55>
2020-11-08 13:54:08 +0900

    api

--- test_fetch_address.py
+++ test_fetch_address.py
@@ -1,2 +1,17 @@
-def test_8480046_佐賀県伊万里市伊万里町乙():
+import requests
+
+
+def fetch_address(zipcode: str) -> str:
+    url = f'https://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}'
+
+    response = requests.get(url)
+
+    addess1 = response.json()['results'][0]['address1']
+    addess2 = response.json()['results'][0]['address2']
+    addess3 = response.json()['results'][0]['address3']
+
+    return response.json()['results']
+
+
+def test_8480046_佐賀県伊万里市伊万里町乙である():
     assert fetch_address(zipcode='8480046') == '佐賀県伊万里市伊万里町乙'
