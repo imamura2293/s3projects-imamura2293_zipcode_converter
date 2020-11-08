324c34e9fbba065fff7f6cdd065b4a04ac052546
imamura2293 <uu77ee22dd55>
2020-11-08 14:41:34 +0900

    tuika

--- test_fetch_address.py
+++ test_fetch_address.py
@@ -9,11 +9,12 @@ def fetch_address(zipcode: str) -> str:
     #郵便番号はすべて、1つの地名に紐づくものとする。
     result  = response.json()['results'][0]
 
-    addess1 = result['address1']
-    addess2 = result['address2']
-    addess3 = result['address3']
 
-    return f'{addess1}{addess2}{addess3}'
+    都道府県名 = result['address1']
+    市区町村 = result['address2']
+    町域名 = result['address3']
+
+    return f'{都道府県名}{市区町村名}{町域名}'
 
 
 def test_8480046_佐賀県伊万里市伊万里町乙である():
